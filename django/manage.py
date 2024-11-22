1e564cc9cbd4a32bfe245e71564f1364464db844
Sat <sat.philora@gmail.com>
2024-11-22 14:09:06 +0100

    chore(dev-env): improve development environment setup
    This commit consolidates several changes to facilitate local development, allow debuggers and improve stability.
    
    - Allow debugpy when running tests
    - Allow debugpy when running locally
    - Allow Vue dev tools for local development.
    - Replace node-sass package with sass package
    - Automatically reset database sequences after restore
    - Use init container to improve host settings for redis
    - Run on non-privileged port
    - Minor tweaks for stability

--- django/manage.py
+++ django/manage.py
@@ -2,9 +2,26 @@
 import os
 import sys
 
+
+# Start the debugpy debugger if not already running
+def start_debugger():
+    try:
+        from django.conf import (
+            settings,  # Import settings after setting DJANGO_SETTINGS_MODULE
+        )
+        if settings.DEBUG:
+            if os.environ.get('RUN_MAIN'): # Will only return true if this is the main process of djangos runserver
+                import debugpy
+                debugpy.listen(5678)
+                print("Debugpy listening on port 5678")
+    except ImportError:
+        print("debugpy not installed or settings not configured. Skipping debug server setup.")
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tiip.settings")
 
-    from django.core.management import execute_from_command_line
+    # Start debugger if needed
+    start_debugger()
 
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
